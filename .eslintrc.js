06655d05c9bc6992e124b789d0ae2cd60df3264a
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-08-24 14:10:14 +0200

    add storybook, react, disiz

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
-    "root": true,
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint",
-    ],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    "rules": {
-        "no-extra-boolean-cast": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-    },
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint"],
+  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
+  "rules": {
+    "no-extra-boolean-cast": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  }
 };
