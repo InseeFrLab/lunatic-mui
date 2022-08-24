81c7c15475626a853fa187453e04c328d37002dd
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-08-24 14:39:34 +0200

    add storybook theming

---
+++ .storybook/customTheme.js
@@ -0,0 +1,38 @@
+import { create } from "@storybook/theming";
+
+export const darkTheme = create({
+    "base": "dark",
+    "brandTitle": "Lunatic MUI",
+    "brandUrl": "https://github.com/ddecrulle/lunatic-mui",
+
+    "appBg": "#0A192E",
+    "appContentBg": "#0A192E",
+
+    "barBg": "#0A192E",
+
+    "colorSecondary": "#FBD616",
+
+    "textColor": "#f1f0eb",
+    "textInverseColor": "#ffc0cb",
+
+    "fontBase": '"Montserrat", sans-serif',
+    "fontCode": "monospace",
+});
+
+export const lightTheme = create({
+    "base": "light",
+    "brandTitle": "Lunatic MUI",
+    "brandUrl": "https://github.com/ddecrulle/lunatic-mui",
+
+    "appBg": "#F5F7FA",
+    "appContentBg": "#F5F7FA",
+    "barBg": "#F5F7FA",
+
+    "colorSecondary": "#3A4657",
+
+    "textColor": "#0F417A",
+    "textInverseColor": "#ffc0cb",
+
+    "fontBase": '"Montserrat", sans-serif',
+    "fontCode": "monospace",
+});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+import { customViewPorts } from "./customViewPorts";
+import { darkTheme, lightTheme } from "./customTheme";
+import { DocsContainer } from "./DocsContainer";
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    "actions": { argTypesRegex: "^on[A-Z].*" },
+    "controls": {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+    "backgrounds": { disable: true },
+    "darkMode": {
+        "stylePreview": true,
+        "light": lightTheme,
+        "dark": darkTheme,
+    },
+    "viewport": {
+        "viewports": customViewPorts,
+    },
+    "docs": {
+        container: DocsContainer,
+    },
+};
