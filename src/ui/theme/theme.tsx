df4178fbabeabede41598183be5ebae3fd582d63
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-08-30 18:17:46 +0200

    Update theme.tsx

--- src/ui/theme/theme.tsx
+++ src/ui/theme/theme.tsx
@@ -3,13 +3,12 @@ import { createButton } from "disiz/Button";
 import { createMakeStyles } from "tss-react/compat";
 import { Checkbox } from "disiz/Checkbox";
 import { TextField } from "disiz/TextField";
-import { createText } from 'disiz/Text';
+import { createText } from "disiz/Text";
 
-const { ThemeProvider, useTheme } = createThemeProvider({})
-
+const { ThemeProvider, useTheme } = createThemeProvider({});
 
 export { ThemeProvider };
-export const { makeStyles, useStyles } = createMakeStyles({ useTheme })
+export const { makeStyles, useStyles } = createMakeStyles({ useTheme });
 export const { Button } = createButton();
 export { Checkbox };
 export { TextField };
