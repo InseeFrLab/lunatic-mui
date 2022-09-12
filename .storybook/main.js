c85da2d33d5aba6e570576005c392c3773c311ab
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-09-12 13:39:53 +0200

    a valide storybook webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,83 @@
 const path = require("path");
+const modulesDir = path.join(process.cwd(), "node_modules");
+
+// This config is due to lunatic : scss and antlr for fallback
+const WebpackConfigOverride = config => ({
+    ...config,
+    "module": {
+        ...config.module,
+        "rules": [
+            ...config.module.rules,
+            {
+                "test": /\.scss$/,
+                "use": ["style-loader", "css-loader", "sass-loader"],
+                "include": path.join(modulesDir, "../"),
+            },
+        ]
+    },
+    "resolve": {
+        ...config.resolve,
+        "modules": [...(config.resolve.modules || []), path.resolve(modulesDir, "../src")],
+        "fallback": {
+            ...(config.resolve || {}).fallback,
+            fs: false,
+            stream: false,
+            os: false,
+        },
+        "alias": {
+            ...config.resolve.alias,
+            "@emotion/core": path.join(modulesDir, "@emotion/react"),
+            "@emotion/styled": path.join(modulesDir, "@emotion/styled"),
+            "@emotion/styled-base": path.join(modulesDir, "@emotion/styled"),
+            "emotion-theming": path.join(modulesDir, "@emotion/react"),
+        },
+    },
+});
+
+const LunaticConfig = async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "../"),
+    });
+
+    config.resolve = {
+        modules: [...(config.resolve.modules || []), path.resolve(__dirname, "../")],
+        fallback: {
+            ...(config.resolve || {}).fallback,
+            fs: false,
+            stream: false,
+            os: false,
+        },
+        plugins: [
+            ...(config.resolve.plugins || []),
+            new TsconfigPathsPlugin({
+                extensions: config.resolve.extensions,
+            }),
+        ],
+    };
+
+    // Return the altered config
+    return config;
+};
 
 module.exports = {
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: [
+    "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    "addons": [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
         "storybook-dark-mode",
     ],
-    framework: "@storybook/react",
-    core: {
+    "framework": "@storybook/react",
+    "core": {
         "builder": "webpack5",
     },
-    webpackFinal: async (config, { configType }) => {
-        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-        // You can change the configuration based on that.
-        // 'PRODUCTION' is used when building the static version of storybook.
-
-        // Make whatever fine-grained changes you need
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-            include: path.resolve(__dirname, "../"),
-        });
-        // Return the altered config
-        return config;
-    },
+    "managerWebpack": updateEmotionAliases,
+    "webpackFinal": updateEmotionAliases,
 };
